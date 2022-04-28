fb5bc2d2cb5e0fe5617b914716276d216745385f
bmunene404@gmail.com <bmunene404@gmail.com>
2022-04-28 12:10:11 +0300

    reverse _ip_update

---
+++ insides/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+  __                      __  
+ /__)_    _ _  _ _  __  //__) 
+/ ( (- \/(-/ _) (-     (/     
+                              
+.                    brian:)
+"""
+
+from insides.colors import *
+from insides.functions import *
+from insides.banner import banner as Banner
+from insides.footer import footer as Footer
---
+++ insides/footer.py
@@ -0,0 +1,23 @@
+"""
+
+  __                      __  
+ /__)_    _ _  _ _  __  //__) 
+/ ( (- \/(-/ _) (-     (/     
+                              
+ :)
+"""
+
+from insides.colors import *
+
+footer = """
+  {white}[{green}${white}] {cyan}Thanks For Using {white}:D
+\t{blue}~ {red}brian:) {white}:)
+""".format(
+        white = w,
+        green = g,
+        cyan = c,
+        blue = b,
+        red = r
+    )
+
+# print footer
